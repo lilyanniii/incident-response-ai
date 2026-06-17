fa93b3a51e6697243a704c2509c088bd33d7e3f9
Gianni Bautista <giannibautista9@gmail.com>
2026-06-16 21:53:12 -0400

    added logic to generate new ID if user decides to insert a new incident

--- vector_storage.py
+++ vector_storage.py
@@ -5,7 +5,7 @@ chroma_client = chromadb.Client()
 collection = chroma_client.create_collection(name="incidents")
 
 def store_incidents_in_db():
-    incidents = generate_incidents(200)
+    incidents = generate_incidents(10)
     for incident in incidents:
         collection.add(
             ids = [incident.id],
@@ -21,3 +21,25 @@ def get_similar_incidents(user_query):
     )
 
     return results['documents'][0], results['metadatas'][0]
+
+def get_last_incident():
+    data = collection.get()
+    ids = data["ids"]
+    max_id= 0
+    for id in ids:
+        string_num_array = id.split("-")
+        str_number = string_num_array[1]
+        number = int(str_number)
+
+        if number > max_id:
+            max_id = number
+    
+    new_Id = max_id + 1
+    formatted_Id = f"INC-{str(new_Id).zfill(4)}"
+    results = formatted_Id
+
+    return results
+
+if __name__ == "__main__":
+    store_incidents_in_db()
+    get_last_incident()
